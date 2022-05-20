ebeddacfd41cbb6e03719dba3da1a52452cd2aba
ktakimoto <takimoto.kengo.dev@gmail.com>
2022-05-20 09:51:34 +0900

    add: #83 apply prettier and stylelint

---
+++ .prettierrc.js
@@ -0,0 +1,22 @@
+// cf.) https://prettier.io/docs/en/options.html
+module.exports = {
+  arrowParens: 'always',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  embeddedLanguageFormatting: 'auto',
+  endOfLine: 'lf',
+  htmlWhitespaceSensitivity: 'css',
+  insertPragma: false,
+  jsxSingleQuote: false,
+  printWidth: 100,
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  rangeEnd: Infinity,
+  rangeStart: 0,
+  requirePragma: false,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
+}
